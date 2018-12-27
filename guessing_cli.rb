8d7137dd2e67107ce7b9967f9695e152caaccfbd
Rene Ventura <venturar729@gmail.com>
2018-12-27 05:34:48 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,4 +3,10 @@ def run_guessing_game
   generated_num = rand(1..6)
   puts "Guess a number between 1 and 6."
   guess = gets.strip
+  while guess != "exit"
+    if guess.to_i == generated_num
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{generated_num}."
+  end
 end
